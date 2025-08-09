89316271d976583eaaf4c12d1a0868f02b9c8cf0
Shivam Sharma <meshivam81@gmail.com>
2025-08-09 08:52:36 +0530

    fix: improve layout sleep tracker

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="border-t-2 border-gray-200">
+    <footer>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           {/* Logo and Tagline */}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default async function Navbar() {
  
   return (
     <nav>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 border-b-2 border-gray-200">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="flex-shrink-0">
